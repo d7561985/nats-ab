d902d6148566748b6adbb50ef5f3432b9b3a68b5
Dmitry Harupa <d.harupa@pin-up.team>
2022-07-11 12:21:08 +0200

    misc: rid of unnecessary options

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"time"
-
-	"github.com/nats-io/nats.go"
 )
 
 type Nats struct {
@@ -20,37 +18,9 @@ type Nats struct {
 	Client      Cred
 	StreamAdmin Cred
 	SysAdmin    Cred
-
-	opts []nats.Option
-}
-
-type Option interface {
-	apply(*Nats)
-}
-
-type XX func(*Nats)
-
-func (x XX) apply(in *Nats) {
-	x(in)
-}
-
-func newConfig(opts ...Option) *Nats {
-	s := &Nats{}
-
-	for _, opt := range opts {
-		opt.apply(s)
-	}
-
-	return s
 }
 
 type Cred struct {
 	User         string
 	UserPassword string
-}
-
-func WithNatsOptions(opts ...nats.Option) Option {
-	return XX(func(n *Nats) {
-		n.opts = append(n.opts, opts...)
-	})
 }
